358793b4dc562d9f156833e083c22d4db557d627
Salahslaoui <salahslaoa@gmail.com>
2025-10-10 19:16:03 +0100

    update 09

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -16,27 +16,22 @@ bool compareFront(const std::vector<int>& a, const std::vector<int>& b)
     return a.front() < b.front();
 }
 
-size_t fill_save(std::vector<std::vector<int> > &tmp, std::vector<std::vector<int> > &save, size_t pad)
+void fill_save(std::vector<std::vector<int> > &tmp, std::vector<std::vector<int> > &save)
 {
-    int j = 0;
     size_t pads = 0;
-    for (size_t i = 0; i < save.size();)
+    for (size_t i = 0; i < save.size(); ++i)
     {
-        j+= pads;
-        std::vector<std::vector<int> >::iterator pos = std::lower_bound(tmp.begin(), tmp.begin() + pads, save[i], compareFront);
+        std::vector<std::vector<int> >::iterator pos = std::lower_bound(tmp.begin(), tmp.begin() + pads + i, save[i], compareFront);
         tmp.insert(pos, save[i]);
         pads++;
-        save.erase(save.begin() + i);
-        j++;
-    }
-    return pad;
+    };
 }
 
 void    merge_container(std::vector<std::vector<int> > &tmp, std::vector<std::vector<int> > &leftover)
 {
     size_t pair_size = 0;
     std::vector<std::vector<int> > save;
-    size_t pad = 0;
+
     // std::vector<int> split;
 
     // for (size_t i = 1; i <tmp.size(); ++i)
@@ -77,27 +72,8 @@ void    merge_container(std::vector<std::vector<int> > &tmp, std::vector<std::ve
         tmp[j].erase(tmp[j].begin() + pair_size, tmp[j].end());
         save.push_back(split);
     }
-    pad = fill_save(tmp, save, pad);
-    for (size_t j = 0; j < tmp.size(); ++j)
-    {
-        if (tmp[j].size() == 2)
-        {
-            pair_size = 1;
-            std::vector<int> split(tmp[j].begin() + pair_size, tmp[j].end());
-            tmp[j].erase(tmp[j].begin() + pair_size, tmp[j].end());
-            save.push_back(split);
-        }
-    }
-    pad = fill_save(tmp, save, pad);
+    fill_save(tmp, save);
 }
-
-// void    split_insert(std::vector<std::vector<int> > &tmp, size_t save)
-// {
-//     for (size_t i = 0; i < save; ++i)
-//     {
-
-//     }
-// }
 
 std::vector<std::vector<int> > pair_merge(char **av)
 {
@@ -116,18 +92,18 @@ std::vector<std::vector<int> > pair_merge(char **av)
 
         std::vector<int> p;
 
-        if (av[i + 1])
-        {
-            check_av(av[i + 1]);
-            int b = std::atoi(av[i + 1]);
-            if (a > b)
-                p.push_back(a), p.push_back(b);
-            else
-                p.push_back(b), p.push_back(a);
-            i++;
-        }
-        else 
-            p.push_back(a);
+        // if (av[i + 1])
+        // {
+        //     check_av(av[i + 1]);
+        //     int b = std::atoi(av[i + 1]);
+        //     if (a > b)
+        //         p.push_back(a), p.push_back(b);
+        //     else
+        //         p.push_back(b), p.push_back(a);
+        //     i++;
+        // }
+        // else 
+        p.push_back(a);
         tmp.push_back(p);
     }
     save = tmp.size();
